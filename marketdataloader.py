84fddf718d15fecaaba2dfbecd75d183f8ffbbed
lfraundorfer <141073723+lfraundorfer@users.noreply.github.com>
2025-07-23 09:12:14 +0200

    Decouple check of input validity and index computation in marketdataloader, re-order validity checks so correct error is raised (isintance before positivity, because <0 would raise an error on e.g. a string, breaking my tests).

--- marketdataloader.py
+++ marketdataloader.py
@@ -1,11 +1,44 @@
 # Receives a random seed (integer) and a timeinterval(int), returns a list of (seed+timeinterval) closing prices (floats)
 
+from datacleaner import CleanRow
+
 class MarketDataLoader:
-    def __init__(self, clean_data: list, seed:int, time_interval:int):
+    def __init__(self, clean_data: list, seed:int, game_length:int):
         self.cleandata = clean_data
         self.seed = seed
-        self.timeinterval = time_interval
+        self.gamelength = game_length
 
-    def load(self):
+    def _compute_indices(self):
+        start_index = self.seed
+        end_index = start_index + self.gamelength +1 #slicing excludes the last index, hence the +1
+        if end_index > len(self.cleandata):
+            raise ValueError(f"Game length out of bounds.")
+        if self.seed > len(self.cleandata):
+            raise ValueError(f"Seed out of bounds.")
 
-        return self.cleandata
+        return (start_index, end_index)
+    
+    def _check_input_validity(self):
+        if not isinstance(self.seed, int):
+            raise ValueError("Seed must be an integer.")
+        if not isinstance(self.gamelength, int):
+            raise ValueError("Game length must be an integer.")
+        if self.seed < 0:
+            raise ValueError("Seed should be positive.")
+        if self.gamelength <= 0:
+            raise ValueError("Game length should be greater than 0.")
+
+    def load(self) -> list[CleanRow]:
+        """
+        Returns a slice of cleaned data based on seed and game_length.
+        """
+        self._check_input_validity()
+        start_index, end_index = self._compute_indices()
+        try:
+            sliced_clean_data = self.cleandata[start_index:end_index]
+        except TypeError as e:
+            raise TypeError(f"Can not use {e} as seed / gamelength")
+        # except IndexError as e:
+        #     raise IndexError(f"Game length out of bounds.")
+        #this does not raise an error because slicing just automatically stops at the last index
+        return sliced_clean_data
--- tests/test_marketdataloader.py
+++ tests/test_marketdataloader.py
@@ -1,9 +1,73 @@
 import pytest
 from marketdataloader import MarketDataLoader
-from datacleaner import DataCleaner
+from datacleaner import DataCleaner, CleanRow
+
+clean_data = DataCleaner("tests/test_csv_files/btc_test_data.csv").clean_csv()
+market_data = MarketDataLoader(clean_data = clean_data, seed=1, game_length=1).load()
 
 
-def test_returns_list():
+def test_returns_nonempty_list():
     clean_data = DataCleaner("tests/test_csv_files/btc_test_data.csv").clean_csv()
-    market_data = MarketDataLoader(clean_data = clean_data, seed=1, time_interval=1).load()
+    market_data = MarketDataLoader(clean_data = clean_data, seed=1, game_length=1).load()
     assert isinstance(market_data, list)
+    assert len(market_data) > 0
+
+def test_list_is_correct_length():
+    seed = 1
+    game_length = 2
+    market_data = MarketDataLoader(clean_data, seed, game_length).load()
+    assert len(market_data) == int(seed + game_length)
+
+def test_raise_on_nonnumeric_game_length():
+    with pytest.raises(ValueError):
+        seed = 1
+        game_length = "one"
+        MarketDataLoader(clean_data, seed, game_length).load()
+
+def test_raise_on_nonnumeric_seed():
+    with pytest.raises(ValueError):
+        seed = "two"
+        game_length = 1
+        MarketDataLoader(clean_data, seed, game_length).load()
+
+def test_raise_on_negative_game_length():
+    with pytest.raises(ValueError):
+        seed = 1
+        game_length = "one"
+        MarketDataLoader(clean_data, seed, game_length).load()
+
+def test_raise_on_negative_seed():
+    with pytest.raises(ValueError):
+        seed = "two"
+        game_length = 1
+        MarketDataLoader(clean_data, seed, game_length).load()
+
+def test_raise_on_float_seed():
+    with pytest.raises(ValueError):
+        seed = 1.0
+        game_length = 1
+        MarketDataLoader(clean_data, seed, game_length).load()
+
+def test_raise_on_float_game_length():
+    with pytest.raises(ValueError):
+        seed = 1
+        game_length = 1.0
+        MarketDataLoader(clean_data, seed, game_length).load()
+
+def test_raise_on_game_length_out_of_bounds():
+    with pytest.raises(ValueError):
+        seed = 1
+        game_length = 999999
+        MarketDataLoader(clean_data, seed, game_length).load()
+
+def test_raise_on_seed_out_of_bounds():
+    with pytest.raises(ValueError):
+        seed = 9999999
+        game_length = 1
+        MarketDataLoader(clean_data, seed, game_length).load()
+
+def test_returns_list_of_CleanRow():
+    clean_data = DataCleaner("tests/test_csv_files/btc_test_data.csv").clean_csv()
+    market_data = MarketDataLoader(clean_data = clean_data, seed=1, game_length=1).load()
+    for daily_data in market_data:
+        assert isinstance(daily_data, CleanRow)
